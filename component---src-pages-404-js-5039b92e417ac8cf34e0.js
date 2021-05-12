cd2f965641b508aaecc95dadf1525cdbca9b0b4a
unicornlab-alphinC <64014583+unicornlab-alphinC@users.noreply.github.com>
2021-05-12 15:37:59 +0400

    initial built files

---
+++ component---src-pages-404-js-5039b92e417ac8cf34e0.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(3751),u=a(5378);t.default=function(){return l.createElement("div",null,l.createElement(n.Z,{title:"404: Not found"}),l.createElement(u.Z,{siteTitle:"Kiara Million"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5039b92e417ac8cf34e0.js.map
---
+++ component---src-pages-404-js-f665e59e1d9f6654ea6c.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(3751),u=a(1088);t.default=function(){return l.createElement("div",null,l.createElement(n.Z,{title:"404: Not found"}),l.createElement(u.Z,{siteTitle:"Kiara Million"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f665e59e1d9f6654ea6c.js.map
