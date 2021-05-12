44fcb9f1bd7f4df1c41dfba005ced65dfaa422a6
BetaLixT <alp2.qwe@gmail.com>
2021-05-13 03:16:50 +0400

    audio and message updates

--- component---src-pages-404-js-71b14ebd2456701bfa49.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(3751),u=a(6285);t.default=function(){return l.createElement("div",null,l.createElement(n.Z,{title:"404: Not found"}),l.createElement(u.Z,{siteTitle:"Kiara Million"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-71b14ebd2456701bfa49.js.map
